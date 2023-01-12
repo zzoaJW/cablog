308cbc10eccd1ad554d7d466a066d903b054e48e
z(0o0a) <i7can7i@gmail.com>
2023-01-13 02:27:47 +0900

    JSX 문법 3개

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = '강남 우동 맛집'
+
   return (
     <div className="App">
-      <div>
-        <h4>블로그임</h4>
+      <div className='black-nav'>
+        <h4 style={{color:'red', fontSize:'16px'}}>블로그임</h4>
       </div>
+        <h4>{post}</h4>
     </div>
   );
 }
