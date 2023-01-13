85e66e6fbc25c2916b366805fa19729f2001c402
z(0o0a) <i7can7i@gmail.com>
2023-01-13 17:10:16 +0900

    main 변경

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <div className='black-nav'>
-        <h4 style={{color:'red', fontSize:'16px'}}>블로그임</h4>
+        <h4 style={{color:'yellow', fontSize:'16px'}}>블로그임</h4>
       </div>
         <h4>{post}</h4>
     </div>
