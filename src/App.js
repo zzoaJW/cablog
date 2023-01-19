b6699dfbb1c56737cdd3937203fd8aa3a7ecc1c6
z(0o0a) <i7can7i@gmail.com>
2023-01-19 22:34:52 +0900

    state, onClick

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집'
+  let post = '강남 우동 맛집';
+  let [글제목, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className='black-nav'>
-        <h4 style={{color:'red', fontSize:'16px'}}>블로그임</h4>
+        <h4>React Bolg <button onClick={()=>{setTitle(['여자 코트 추천', '강남 우동 맛집', '파이썬 독학'])}}>여자로 바꾸기</button></h4>
       </div>
-        <h4>{post}</h4>
+      <div className="list">
+        <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span>{따봉}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
