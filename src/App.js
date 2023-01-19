02ebf04e578f7269e0c9b3625da292bb8b45ac9a
z(0o0a) <i7can7i@gmail.com>
2023-01-19 23:50:53 +0900

    array spread copy, component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className='black-nav'>
-        <h4>React Bolg <button onClick={()=>{setTitle(['여자 코트 추천', '강남 우동 맛집', '파이썬 독학'])}}>여자로 바꾸기</button></h4>
+        <h4>React Bolg</h4>
       </div>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy);
+      }}>가나다순 정렬</button>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+        }}>여자로 바꾸기</button>
+
       <div className="list">
         <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span>{따봉}</h4>
         <p>2월 17일 발행</p>
@@ -24,8 +37,21 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal/>
+
     </div>
   );
+}
+
+function Modal(){
+  return(
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
