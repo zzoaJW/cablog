41291c8e5c41bead0420480377ffac78fec3fd9d
z(0o0a) <i7can7i@gmail.com>
2023-01-21 21:38:01 +0900

    map, props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { useState } from 'react';
 function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
+  let [선택글인덱스, 인덱스변경] = useState(0);
   let [modal, setModal] = useState(false);
 
   return (
@@ -25,33 +26,45 @@ function App() {
         글제목변경(copy);
         }}>여자로 바꾸기</button>
 
-      <div className="list">
-        <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span>{따봉}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4 onClick={()=>{setModal(!modal)}}>{글제목[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
+      {
+        글제목.map(function(a, i){
+          return (      
+            <div className="list" key={i}>
+              <h4 onClick={ ()=>{
+                  인덱스변경(i)
+                  setModal(true)
+                } }>
+                { 글제목[i] }
+                <span onClick={()=>{
+                  let copy = [...따봉];
+                  copy[i]++;
+                  따봉변경(copy)
+                  }}>👍</span>
+                {따봉[i]}
+                </h4>
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      }
+
+      <input type="range"/>
 
       {
-        modal? <Modal/> : null
+        modal? <Modal 선택글인덱스={선택글인덱스} 글제목={글제목}/> : null
       }
 
     </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return(
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[props.선택글인덱스]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button>글 수정</button>
     </div>
   )
 }
