5de556df1e44c0d984f3fd4e39bae6bb09a3f534
z(0o0a) <i7can7i@gmail.com>
2023-01-22 10:52:49 +0900

    state array add, delete

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { useState } from 'react';
 function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
-  let [따봉, 따봉변경] = useState([0, 0, 0]);
+  let [따봉, 따봉변경] = useState(글제목.map(function(){
+    return (0)
+  }));
   let [선택글인덱스, 인덱스변경] = useState(0);
   let [modal, setModal] = useState(false);
+  let [입력값, 입력값변경] = useState('');
 
   return (
     <div className="App">
@@ -30,12 +33,14 @@ function App() {
         글제목.map(function(a, i){
           return (      
             <div className="list" key={i}>
+              <div>
               <h4 onClick={ ()=>{
-                  인덱스변경(i)
-                  setModal(true)
+                  인덱스변경(i);
+                  setModal(true);
                 } }>
                 { 글제목[i] }
-                <span onClick={()=>{
+                <span onClick={(e)=>{
+                  e.stopPropagation();
                   let copy = [...따봉];
                   copy[i]++;
                   따봉변경(copy)
@@ -43,12 +48,22 @@ function App() {
                 {따봉[i]}
                 </h4>
               <p>2월 17일 발행</p>
+              </div>
+              <button onClick={()=>{
+                let copy = [...글제목];
+                copy.splice(i, 1)
+                글제목변경(copy)
+              }}>글 삭제</button>
             </div>
           )
         })
       }
 
-      <input type="range"/>
+      <input onChange={(e)=>{
+        입력값변경(e.target.value);
+        }}/> <button onClick={()=>{
+          글제목변경(글제목.concat(입력값));
+          }}>글 발행</button>
 
       {
         modal? <Modal 선택글인덱스={선택글인덱스} 글제목={글제목}/> : null
